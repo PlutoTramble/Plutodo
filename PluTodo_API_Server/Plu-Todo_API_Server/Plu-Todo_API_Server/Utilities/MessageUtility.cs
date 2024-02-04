a324a1e3f63dff7b0709589ec2cc79b97c2ecb15
PlutoTramble <cedric.a.t@protonmail.com>
2024-02-04 00:06:19 -0500

    Remove dotnet version

--- PluTodo_API_Server/Plu-Todo_API_Server/Plu-Todo_API_Server/Utilities/MessageUtility.cs
+++
@@ -1,6 +0,0 @@
-namespace Plu_Todo_API_Server.Utilities;
-
-public static class MessageUtility
-{
-    public static string Out(OutputMessages value) => Enum.GetName(typeof(OutputMessages), value)!;
-}
