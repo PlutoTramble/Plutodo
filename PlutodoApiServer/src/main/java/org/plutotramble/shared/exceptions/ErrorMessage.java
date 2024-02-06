d066cdd88aed69e7c179d51d951c95a7180a2d95
PlutoTramble <cedric.a.t@protonmail.com>
2024-02-05 20:21:31 -0500

    Improve exception and error handling, this time with files.

---
+++ PlutodoApiServer/src/main/java/org/plutotramble/shared/exceptions/ErrorMessage.java
@@ -0,0 +1,8 @@
+package org.plutotramble.shared.exceptions;
+
+public class ErrorMessage {
+    public ErrorMessage(String message){
+        error = message;
+    }
+    String error;
+}
---
+++ PlutodoApiServer/src/main/java/org/plutotramble/shared/exceptions/InvalidItemPropertyException.java
@@ -0,0 +1,7 @@
+package org.plutotramble.shared.exceptions;
+
+public class InvalidItemPropertyException extends Exception {
+    public InvalidItemPropertyException(String msg){
+        super(msg);
+    }
+}
